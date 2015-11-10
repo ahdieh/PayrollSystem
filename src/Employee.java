dcccc18edfa9f273be1397c842afbc2c13263ddd
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-10 15:48:21 -0600

    complete Employee (abstract) class

--- src/Employee.java
+++ src/Employee.java
@@ -5,4 +5,41 @@ public abstract class Employee {
 	private String _firstName;
 	private String _lastName;
 	private String _SSN;
+	
+	// CONSTRUCTOR ++++++++++++++++++++++
+	
+	public Employee(){
+		_initialize("","","");
+	}
+	
+	//PRIVATE METHODS ++++++++++++++++++
+	
+	private void _initialize(String firstName, String lastName, String SSN){
+		this._firstName = firstName;
+		this._lastName = lastName;
+		this._SSN = SSN;
+	}
+	
+	//PROPERTIES +++++++++++++++++++++
+	
+	public String getFirstName(){
+		return this._firstName;
+	}
+	
+	public String getLastName(){
+		return this._lastName;
+	}
+	
+	public String getSSN(){
+		return this._SSN;
+	}
+	
+	// PUBLIC METHODS
+	
+	public String toString(){
+		return String.format("%s %s with the social security number: %s", getFirstName(),getLastName(),getSSN());
+	}
+	
+	public abstract double earnings();
+	
 }
