d2e8cd2de79aa711636db00dc70288d4b2b376c6
ahdieh <aemadi@my.centennialcollege.ca>
2015-11-11 02:49:53 -0600

    complete the SalariedEmployee class(subclass of the Employee class)

--- src/SalariedEmployee.java
+++ src/SalariedEmployee.java
@@ -4,4 +4,39 @@ public class SalariedEmployee extends Employee {
 	//PRIVATE VARIABELS +++++++++++++++++++++++
 
 		private double _weeklySalary;
+		
+    // CONSTRUCTOR ++++++++++++++++++++++
+		
+		public SalariedEmployee(String firstName, String lastName, String SSN, double weeklySalary){
+			super(firstName, lastName,SSN);
+			if (this._weeklySalary<0){
+				throw new IllegalArgumentException("The weekly salary must be greater than 0");
+			}
+		this._weeklySalary = weeklySalary;
+			
+			}
+
+			
+		// PROPERTIES ++++++++++++++++++
+		
+		public double getWeeklySalary() {
+			return this._weeklySalary;
+		}
+
+		public void setWeeklySalary(double weeklySalary) {
+			if (this._weeklySalary<0){
+				throw new IllegalArgumentException("The weekly salary must be greater than 0");
+			}
+			this._weeklySalary = weeklySalary;
+		}
+		
+	// PUBLIC METHODS +++++++++++++++++
+		
+		public double earnings(){
+			return getWeeklySalary();
+		}
+		
+		public String toString(){
+			return String.format("%s%nis a salaried employee.%nwith the weekly salary: $%,02f", super.toString(),getWeeklySalary());
+		}
 }
